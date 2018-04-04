e472a6bf1bae71a2b2fd141ccc90e4c1790ed31e
Filip Jerga <filipjerga@filips-mbp.lan>
2018-04-04 11:06:42 +0200

    Add mongoose model

--- server/server.js
+++ server/server.js
@@ -1,14 +1,20 @@
 const express       = require("express"),
       app           = express(),
-      bodyParser    = require("body-parser");
-      // mongoose        = require("mongoose"),
-      // Rental          = require("./models/rental"),
+      bodyParser    = require("body-parser"),
+      mongoose      = require("mongoose"),
+      Rental        = require("./models/rental"),
+      keys          = require("./keys"),
+      fakeDB        = require("./seed-db");
 
 const rentalsRoutes = require("./routes/rentals");
       // authRoutes    = require("./routes/auth"),
 
-// var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp";
-// mongoose.connect(url);
+const url = `mongodb://${keys.DB_USER}:${keys.DB_PASSWORD}@ds163034.mlab.com:63034/bwm-dev`;
+mongoose.connect(url).then(() => {
+  // populate DB
+  fakeDB.seed();
+});
+
 
 app.use(bodyParser.json()); // use od body parser to get values from get req
 
