da27e0650624ba6b05f3e4dd6702f7369e2f1b91
Filip Jerga <filipjerga@filips-mbp.lan>
2018-04-06 10:39:18 +0200

    Adds signin and auth middleware

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,5 +4,10 @@ const User = require("../models/user");
 const Auth = require("../controllers/auth");
 
 router.post("/users", Auth.signup);
+router.post("/auth", Auth.signin);
+
+router.get("/test", Auth.authMiddleware, function(req, res) {
+  res.json({ok: "ok"});
+});
 
 module.exports = router;
