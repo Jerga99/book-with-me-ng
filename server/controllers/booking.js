2e704997c2499d4747e6b671a436da99e91ad241
Filip Jerga <filipjerga@filips-mbp.lan>
2018-04-07 13:38:09 +0200

    Prepares route for create booking and seed DB

---
+++ server/controllers/booking.js
@@ -0,0 +1,5 @@
+const Booking = require("../models/booking");
+
+exports.createBooking = function(req, res, next) {
+
+}
--- server/seed-db.js
+++ server/seed-db.js
@@ -1,5 +1,6 @@
 const Rental = require("./models/rental");
 const User = require("./models/user");
+const Booking = require('./models/booking');
 
 module.exports = (function() {
   const data = {
@@ -33,22 +34,70 @@ module.exports = (function() {
       "description": "Very nice apartment in center of the city.",
       "dailyRate": 23
     }],
-    "user": {username: "Filip99", email: "test@gmail.com", password: "testtest"}
+    "user1": {username: "Filip", email: "filip@gmail.com", password: "filipfilip"},
+    "user2": {username: "Test", email: "test@gmail.com", password: "testtest"},
+    "booking1": {startAt: new Date(2018, 4, 27), endAt: new Date(2018, 4, 29), totalPrice: 100, days: 2, guests: 3},
+    "booking2": {startAt: new Date(2018, 4, 12), endAt: new Date(2018, 4, 17), totalPrice: 299, days: 5, guests: 1},
   }
 
-  function FakeDB() {};
+
+  function FakeDB() {
+    this.booking1 = new Booking(data['booking1']);
+    this.booking2 = new Booking(data['booking2']);
+    this.user1 = new User(data['user1']);
+    this.user2 = new User(data['user2']);
+    this.rental1 = new Rental(data['rentals'][0]);
+    this.rental2 = new Rental(data['rentals'][1]);
+    this.rental3 = new Rental(data['rentals'][2]);
+
+    this.cleanDB = async function() {
+      await User.remove({});
+      await Rental.remove({});
+      await Booking.remove({});
+    }
+
+    this.saveUsers = async function() {
+      this.user1.rentals.push(this.rental1);
+      this.user1.rentals.push(this.rental2);
+      this.user1.rentals.push(this.rental3);
+
+      this.user2.bookings.push(this.booking1);
+      this.user2.bookings.push(this.booking2);
+
+      await this.user1.save();
+      await this.user2.save();
+    }
+
+    this.saveBookings = async function() {
+      this.booking1.user = this.user2;
+      this.booking2.user = this.user2;
+      this.booking1.rental = this.rental1;
+      this.booking2.rental = this.rental1;
+
+      await this.booking1.save();
+      await this.booking2.save();
+    }
+
+    this.saveRentals = async function() {
+      data['rentals'].forEach((rental, index) => {
+        const newRental = new Rental(rental);
+
+        if (index === 0) {
+          newRental.bookings.push(this.booking1);
+          newRental.bookings.push(this.booking2);
+        }
+
+        newRental.user = this.user1;
+        newRental.save();
+      });
+    }
+  };
 
   FakeDB.prototype.seed = async function() {
-    await User.remove({});
-    await Rental.remove({});
-    const user = new User(data['user']);
-    await user.save();
-
-    data['rentals'].forEach(rental => {
-      const newRental = new Rental(rental);
-      newRental.user = user;
-      newRental.save();
-    });
+    await this.cleanDB()
+    await this.saveUsers();
+    await this.saveBookings();
+    await this.saveRentals();
   }
 
   return new FakeDB();
