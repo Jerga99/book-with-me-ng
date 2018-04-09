6a81e1b7415c398dff8cb157e34da4c41d2f2752
Filip Jerga <filipjerga@filips-mbp.lan>
2018-04-09 23:27:01 +0200

    Adds get booking by user on server

--- server/routes/bookings.js
+++ server/routes/bookings.js
@@ -2,7 +2,20 @@ const express = require("express");
 const router = express.Router();
 const Booking = require("../controllers/booking");
 const Auth = require("../controllers/auth");
+const {normalizeErrors} = require("../helpers/mongoose-helper");
 
 router.post('/', Auth.authMiddleware, Booking.createBooking)
+
+router.get('/manage', Auth.authMiddleware, function(req, res) {
+  const user = res.locals.user;
+
+  Booking.where({user: user}, function(err, foundBookings){
+    if (err) {
+      return res.status(422).send({errors: normalizeErrors(err.errors) });
+    }
+
+    res.json(foundBookings);
+  });
+});
 
 module.exports = router;
